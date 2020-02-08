78b89521230bade462bf0848d35e3814735d1b36
onlyifihadtime <kanzadounichway@gmail.com>
2020-02-08 22:32:47 +0100

    english
    english

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDoUZzCvl4xxTCTBmYNGhhsJsuhCkp9FeU",
-    authDomain: "project-net-3051e.firebaseapp.com",
-    databaseURL: "https://project-net-3051e.firebaseio.com",
-    projectId: "project-net-3051e",
-    storageBucket: "project-net-3051e.appspot.com",
-    messagingSenderId: "304716738874",
-    appId: "1:304716738874:web:ca42273d3531fd9c3c2003"
+    apiKey: "AIzaSyCN2vavi6Gf_LVFDiFOC7vvZrkkfRHrByw",
+    authDomain: "netfiix-a7b51.firebaseapp.com",
+    databaseURL: "https://netfiix-a7b51.firebaseio.com",
+    projectId: "netfiix-a7b51",
+    storageBucket: "netfiix-a7b51.appspot.com",
+    messagingSenderId: "903440347263",
+    appId: "1:903440347263:web:fb4841202ada59a2ebcf4a"
   }
 };
 
